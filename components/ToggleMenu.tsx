035d70da80f9c226eaf06b4971bd66957c90b4d5
Paul Van Oosten <paulvoosten@protonmail.com>
2022-06-03 01:27:00 +0200

    Add event hooks

--- components/ToggleMenu.tsx
+++ components/ToggleMenu.tsx
@@ -1,12 +1,15 @@
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { PropsWithChildren, useState } from 'react';
+import { PropsWithChildren, useRef, useState } from 'react';
+import { useOnClickOutside } from '../hooks/events';
 import styles from '../styles/Spotify.module.css';
 
 const ToggleMenu = ({ children }: PropsWithChildren<{}>) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef(null);
+  useOnClickOutside(ref, () => setOpen(false));
   return (
-    <>
+    <div ref={ref}>
       <div
         className={`${styles.toggleMenu}${open ? ' ' + styles.open : ''}`}
         onClick={() => setOpen(!open)}
@@ -14,7 +17,7 @@ const ToggleMenu = ({ children }: PropsWithChildren<{}>) => {
         <FontAwesomeIcon icon={faBars} className={styles.bars} />
       </div>
       {open && <div className={styles.menu}>{children}</div>}
-    </>
+    </div>
   );
 };
 
---
+++ hooks/events.ts
@@ -0,0 +1,58 @@
+import { RefObject, useEffect, useRef } from 'react';
+
+export function useEventListener<
+  KW extends keyof WindowEventMap,
+  KH extends keyof HTMLElementEventMap,
+  T extends HTMLElement | void = void
+>(
+  eventName: KW | KH,
+  handler: (
+    event: WindowEventMap[KW] | HTMLElementEventMap[KH] | Event
+  ) => void,
+  element?: RefObject<T>
+) {
+  // Create a ref that stores handler
+  const savedHandler = useRef(handler);
+
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
+  useEffect(() => {
+    // Define the listening target
+    const targetElement: T | Window = element?.current || window;
+    if (!targetElement || !targetElement.addEventListener) {
+      return;
+    }
+
+    // Create event listener that calls handler function stored in ref
+    const eventListener: typeof handler = (event) =>
+      savedHandler.current(event);
+
+    targetElement.addEventListener(eventName, eventListener);
+
+    // Remove event listener on cleanup
+    return () => {
+      targetElement.removeEventListener(eventName, eventListener);
+    };
+  }, [eventName, element]);
+}
+
+type Handler = (event: MouseEvent | Event) => void;
+
+export function useOnClickOutside<T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
+  handler: Handler,
+  mouseEvent: 'mousedown' | 'mouseup' = 'mousedown'
+): void {
+  useEventListener(mouseEvent, (event) => {
+    const element = ref?.current;
+
+    // Do nothing if clicking ref's element or descendent elements
+    if (!element || element.contains(event.target as Node)) {
+      return;
+    }
+
+    handler(event);
+  });
+}
